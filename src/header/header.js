f088dbb7478c499298019fa7234558cc4dc1248c
Ugur <ugurbukcuoglu@gmail.com>
2022-07-22 22:48:20 +0300

    lazy load landing + fade animation

--- src/header/header.js
+++ src/header/header.js
@@ -1,15 +1,18 @@
 import "./header.css";
+import Fade from "react-reveal/Fade";
 
 export default function Header() {
   return (
-    <div className="header-container">
-      <div className="heading-container">
-        <h1>Best hotels for the best price</h1>
+    <Fade bottom>
+      <div className="header-container">
+        <div className="heading-container">
+          <h1>Best hotels for the best price</h1>
 
-        <a href="#" className="book-button">
-          Book Now
-        </a>
+          <a href="#" className="book-button">
+            Book Now
+          </a>
+        </div>
       </div>
-    </div>
+    </Fade>
   );
 }
