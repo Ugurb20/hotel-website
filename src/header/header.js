843ebbd89e343964fbfc622382eb1ade4adc9714
Ugur <ugurbukcuoglu@gmail.com>
2022-07-22 21:03:50 +0300

    lazy loading

--- src/header/header.js
+++ src/header/header.js
@@ -1,16 +1,20 @@
 import "./header.css";
 import Mynav from "./nav/nav";
+import { Suspense } from "react";
+
 export default function Header() {
   return (
     <div className="header-container">
       <Mynav />
-      <div className="heading-container">
-        <h1>Best hotels for the best price</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="heading-container">
+          <h1>Best hotels for the best price</h1>
 
-        <a href="#" className="book-button">
-          Book Now
-        </a>
-      </div>
+          <a href="#" className="book-button">
+            Book Now
+          </a>
+        </div>
+      </Suspense>
     </div>
   );
 }
