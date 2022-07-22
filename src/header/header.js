9a77d1e45778d870e94a83d29e977c08bd0f77db
Ugur <ugurbukcuoglu@gmail.com>
2022-07-22 21:52:09 +0300

    cover background

--- src/header/header.js
+++ src/header/header.js
@@ -1,17 +1,24 @@
 import "./header.css";
 import Mynav from "./nav/nav";
 import React, { Suspense } from "react";
+import Spinner from "react-bootstrap/Spinner";
+
 import { Image } from "react-bootstrap";
 
 const Imag = React.lazy(() => import("./heading.js"));
 
 export default function Header() {
+  /*
+      <Suspense
+        fallback={<Spinner animation="border" variant="primary" />}
+      >
+        <Imag /> 
+        </Suspense>*/
   return (
     <div className="header-container">
+      <Imag />
       <Mynav />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Imag />
-      </Suspense>
+
       <div className="heading-container">
         <h1>Best hotels for the best price</h1>
 
--- src/header/heading.js
+++ src/header/heading.js
@@ -1,5 +1,20 @@
 import photo from "./headerbackground.jpg";
 
 export default function Heading() {
-  return <img src={photo}></img>;
+  return (
+    <img
+      src={photo}
+      style={{
+        width: "100%",
+        position: "absolute",
+        maxHeight: "100%",
+        display: "inline-block",
+        minHeight: "100%",
+        left: "0",
+        top: "0",
+        objectFit: "cover",
+        zIndex: "-5",
+      }}
+    ></img>
+  );
 }
