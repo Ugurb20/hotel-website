99186dc7afa352849e05fe962536c90062508df3
Ugur <ugurbukcuoglu@gmail.com>
2022-07-22 22:11:50 +0300

    cover background

--- src/header/header.js
+++ src/header/header.js
@@ -3,14 +3,20 @@ import Mynav from "./nav/nav";
 import React, { Suspense } from "react";
 import Spinner from "react-bootstrap/Spinner";
 
-import { Image } from "react-bootstrap";
-
 const Imag = React.lazy(() => import("./heading.js"));
 
 export default function Header() {
   return (
     <div className="header-container">
-      <Suspense fallback={<Spinner animation="border" variant="primary" />}>
+      <Suspense
+        fallback={
+          <Spinner
+            animation="border"
+            variant="primary"
+            className="my-spinner"
+          />
+        }
+      >
         <Imag />
       </Suspense>
       <Mynav />
