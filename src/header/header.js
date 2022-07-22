8a093f72b60049edd3dc428f52ab9bc6dff28178
Ugur <ugurbukcuoglu@gmail.com>
2022-07-22 21:29:04 +0300

    lazy loading

--- src/header/header.js
+++ src/header/header.js
@@ -1,20 +1,24 @@
 import "./header.css";
 import Mynav from "./nav/nav";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
+import { Image } from "react-bootstrap";
+
+const Imag = React.lazy(() => import("./heading.js"));
 
 export default function Header() {
   return (
     <div className="header-container">
       <Mynav />
       <Suspense fallback={<div>Loading...</div>}>
-        <div className="heading-container">
-          <h1>Best hotels for the best price</h1>
-
-          <a href="#" className="book-button">
-            Book Now
-          </a>
-        </div>
+        <Imag />
       </Suspense>
+      <div className="heading-container">
+        <h1>Best hotels for the best price</h1>
+
+        <a href="#" className="book-button">
+          Book Now
+        </a>
+      </div>
     </div>
   );
 }
---
+++ src/header/heading.js
@@ -0,0 +1,5 @@
+import photo from "./headerbackground.jpg";
+
+export default function Heading() {
+  return <img src={photo}></img>;
+}
